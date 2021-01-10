c3fed77ce60c80bd8c6ee74c5fa3eeb39a8af35e
gustavo.jodar <gustavo.jodar@catijr.com.br>
2021-01-10 16:04:48 -0300

    ha

---
+++ task_4/xv6-public/acesso_zero.c
@@ -0,0 +1,12 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main(int argc, char *argv[])
+{
+    char *p_para_adress_0;
+    p_para_adress_0 = 0;
+    printf(1, "Conteudo do endereco 0: %x\n", *p_para_adress_0);
+
+    exit();
+}
